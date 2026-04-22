c0389f61d5a168d0797e6a53df5193b59fdec549
rupert1404 <rupert.hepton@gmail.com>
2026-04-22 16:51:59 +0100

    implementing caching to work in offline mode

--- sw.js
+++ sw.js
@@ -1,4 +1,27 @@
-// Minimal Service Worker
+const cacheName = 'v1';
+
+const assets = [
+  './',
+  './index.html',
+  './app.js',
+  './icon-192.png',
+  './icon-512.png',
+  './localforge.min.js',
+  './manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
-    
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
